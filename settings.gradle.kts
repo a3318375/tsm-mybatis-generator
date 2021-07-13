4ac47e664342420fc201863160d3c0abe2c7d1c3
GitHub Action <action@github.com>
2021-07-13 03:28:11 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "tsm-mybatis-generator"
---
+++ src/main/kotlin/com/github/a3318375/tsmmybatisgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.a3318375.tsmmybatisgenerator.services
+
+import com.github.a3318375.tsmmybatisgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
